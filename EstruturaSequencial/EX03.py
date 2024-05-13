016962a2d14b3e936acd3ba7fbf29da881c23686
Felipe Almeida <falmeidasant@gmail.com>
2024-05-13 14:31:25 -0300

    Create EX03.py

---
+++ EstruturaSequencial/EX03.py
@@ -0,0 +1,16 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+
+print("Esse é um programa para realizarmos a soma de dois números ;)")
+
+# Solicita que seja digitado um número decimal
+numero1 = float(input("Digite um número: "))
+
+# Solicita que seja digitado um número decimal à ser somado com o anterior
+numero2 = float(input("Digite um número para ser somado com o digitado anteriormente: "))
+
+#define que a váriável resultado é a soma dos dois números solicitados anteriormente
+resultado = numero1+numero2
+
+# Retorna soma dos numeros escolhidos
+print(f"A soma dos números escolhidos é {resultado}")
