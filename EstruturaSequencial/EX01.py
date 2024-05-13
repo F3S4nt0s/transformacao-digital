67033ce72b02014e94af3aecdd756e68ea91ffb4
Felipe Almeida <falmeidasant@gmail.com>
2024-05-13 14:19:15 -0300

    Inclusão enunciados
    finalização ex 1 e 2

--- EstruturaSequencial/EX01.py
+++ EstruturaSequencial/EX01.py
@@ -1,1 +1,3 @@
+#Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
 print("Alô, Mundo!")
--- EstruturaSequencial/EX02.py
+++ EstruturaSequencial/EX02.py
@@ -1,1 +1,8 @@
-print("Quantos anos você tem?")
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. # type: ignore
+
+# Solicita que seja inserido um numero inteiro
+numero = int(input("Digite um número inteiro: "))
+
+
+# exibe a mensagem com o número inserido
+print(f"O número escolhido foi {numero}")
